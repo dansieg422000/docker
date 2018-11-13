e5d462ee9357a95fa3e83a1536054416e96a24e3
dansieg422000 <dansieg422000@yahoo.com>
2018-11-13 12:18:34 +1100

    Add a custom Dockerfile

---
+++ Dockerfile/entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Ensure all users can see the DOCKER environment vars
+# Neede if you want to perform actions as www-data
+env | grep _ >> /etc/environment
+
+# Apache can't cope with memory shared files existing
+rm -f /var/run/apache2/*
+rm -rf /tmp/*
+
+# Start Filebeat (nohup to run in fork)
+#sudo nohup /etc/init.d/filebeat start &
+
+# Run apache in foreground
+apache2ctl -D FOREGROUND
